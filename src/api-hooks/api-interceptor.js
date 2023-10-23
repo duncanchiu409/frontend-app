8552203ceaae24ab4d6f986980dd5b1da7b7a66d
Nik <nikitamittal2423@gmail.com>
2023-10-23 18:53:58 +0530

    Frontend updated

--- src/api-hooks/api-interceptor.js
+++ src/api-hooks/api-interceptor.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `https://app.chainofdemand.co/n/v1`,
+  baseURL: process.env.REACT_APP_API,
   headers: {
     "Content-Type": "application/json",
   },
