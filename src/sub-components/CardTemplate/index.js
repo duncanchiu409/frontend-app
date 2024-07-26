c5aeeb3c68bad5754ae1f1ab552f1c13a27bf14b
duncanchiu409 <chiuduncan409@gmail.com>
2024-07-26 18:35:53 +0800

    Fix: fallback image on broken link

--- src/sub-components/CardTemplate/index.js
+++ src/sub-components/CardTemplate/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./cardTemplate.css"
+import image from '../../treehouse_no_bg_3.png'
 
 const CardTemplate = ({imageSrc,cardContent,contentTitle,contentDate}) => {
     return(
         <div className="card-template">
-            <img src={imageSrc}/>
+            <img src={imageSrc} onError={(e) => e.target.src = image}/>
             <div className="card-content">
                 {cardContent}
                 <div className="date-title">
